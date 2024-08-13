30d0076c5492077f7a63ee106fe2276ca8c90e72
IgnacioMilutin <i.milutin@alumno.um.edu.ar>
2024-08-13 11:45:29 -0300

    Add dockerfile, pieces, chess, board, docker

---
+++ cli.py
@@ -0,0 +1,25 @@
+from chess import Chess
+
+def main():
+    chess=Chess()
+    while True:
+        play(chess)
+
+def play(chess):
+    try:
+        from_row=int(input('From row: ')),
+        from_col=int(input('From col: ')),
+        to_row=int(input('To row: ')),
+        to_col=int(input('to col: '))
+
+        chess.move(from_row,
+                from_col,
+                to_row,
+                to_col)
+    
+    except Exception as e:
+        print('Error')
+
+
+if __name__=='__main__':
+    main()
