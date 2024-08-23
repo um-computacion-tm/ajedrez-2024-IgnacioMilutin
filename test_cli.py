7c6dc2b601e734450588ec76503b654718057e14
IgnacioMilutin <i.milutin@alumno.um.edu.ar>
2024-08-23 12:58:26 -0300

    Created new test for wrong input in cli

--- test_cli.py
+++ test_cli.py
@@ -24,10 +24,30 @@ class TestCli(unittest.TestCase):
         self.assertEqual(mock_print.call_count,3)
         self.assertEqual(mock_chess_move.call_count,0)
 
-    @patch('builtins.input',side_effect=['1','1','2','hola'])
+    @patch('builtins.input',side_effect=['1','hola','2','2'])
     @patch('builtins.print')
     @patch.object(Chess,'move')
     def test_wrong_entry_2(self,mock_chess_move,mock_print,mock_input):
+        chess=Chess()
+        play(chess)
+        self.assertEqual(mock_input.call_count,2)
+        self.assertEqual(mock_print.call_count,3)
+        self.assertEqual(mock_chess_move.call_count,0)
+
+    @patch('builtins.input',side_effect=['1','1','hola','2'])
+    @patch('builtins.print')
+    @patch.object(Chess,'move')
+    def test_wrong_entry_3(self,mock_chess_move,mock_print,mock_input):
+        chess=Chess()
+        play(chess)
+        self.assertEqual(mock_input.call_count,3)
+        self.assertEqual(mock_print.call_count,3)
+        self.assertEqual(mock_chess_move.call_count,0)
+
+    @patch('builtins.input',side_effect=['1','1','2','hola'])
+    @patch('builtins.print')
+    @patch.object(Chess,'move')
+    def test_wrong_entry_4(self,mock_chess_move,mock_print,mock_input):
         chess=Chess()
         play(chess)
         self.assertEqual(mock_input.call_count,4)
@@ -43,3 +63,5 @@ class TestCli(unittest.TestCase):
         self.assertEqual(mock_input.call_count,4)
         self.assertEqual(mock_print.call_count,3)
         self.assertEqual(mock_chess_move.call_count,1)
+
+    
