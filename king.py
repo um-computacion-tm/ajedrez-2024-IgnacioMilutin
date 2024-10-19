ea4d19de9b6b7e05ef60d9e029947114b59fbc6d
IgnacioMilutin <i.milutin@alumno.um.edu.ar>
2024-10-18 22:43:35 -0300

    Updated king valid_postions

--- king.py
+++ king.py
@@ -5,17 +5,22 @@ class King(Piece):
     white_str='♚'
     black_str='♔'
 
+    # FILTERS ALL POSSIBLE KINGS MOVES WITH THE OTHER TEAMS MOVES
+
+    def filter_move(self,possible_positions,all_opponent_moves):
+        final_possible_positions=[]
+        for move in possible_positions:
+            if move not in all_opponent_moves:
+                final_possible_positions.append(move)
+        return final_possible_positions
+    
     # ALL VALID POSITIONS ('for_all_moves' parameter is to avoid recursion when using 'all_moves()' function)
 
     def valid_positions(self,from_row,from_col,for_all_moves=False):
         possible_positions=self.possible_positions_va(from_row,from_col)+self.possible_positions_vd(from_row,from_col)+self.possible_positions_hr(from_row,from_col)+self.possible_positions_hl(from_row,from_col)+self.possible_positions_dar(from_row,from_col)+self.possible_positions_dal(from_row,from_col)+self.possible_positions_ddr(from_row,from_col)+self.possible_positions_ddl(from_row,from_col)
         if for_all_moves==False:
             full_moves=all_moves(self.__board__,self.get_opposite_color())
-            final_possible_positions=[]
-            for move in possible_positions:
-                if move not in full_moves:
-                    final_possible_positions.append(move)
-            return final_possible_positions
+            return self.filter_move(possible_positions,full_moves)
         return possible_positions
 
     # VERTICAL ASCENDANT MOVE
